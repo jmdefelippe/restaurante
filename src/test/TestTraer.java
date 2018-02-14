6299279ab1975f548703121925d2d566ad9a664c
jmdefelippe <jmdefelippe@gmail.com>
2018-02-14 17:44:12 -0300

    tablas camarero y restaurant

--- src/test/TestTraer.java
+++ src/test/TestTraer.java
@@ -1,6 +1,8 @@
 package test;
 
+import negocio.CamareroABM;
 import negocio.LoginABM;
+import negocio.RestaurantABM;
 import negocio.RubroABM;
 import negocio.SubrubroABM;
 import negocio.TipoUsuarioABM;
@@ -13,21 +15,31 @@ public class TestTraer {
 			LoginABM loginABM = new LoginABM();
 			UsuarioABM usuarioABM = new UsuarioABM();
 			
-			RubroABM rubroABM = new RubroABM();
-			SubrubroABM subrubroABM = new SubrubroABM();
-			
 			System.out.println(tipoUsuarioABM.traerTipoUsuario(1));
 			System.out.println();
 			System.out.println(usuarioABM.traerUsuario(1));
 			System.out.println();
 			System.out.println(loginABM.traerLogin(1));
 			System.out.println();
+
+			RubroABM rubroABM = new RubroABM();
+			SubrubroABM subrubroABM = new SubrubroABM();
 			
 			System.out.println(rubroABM.traerRubro(2));
 			System.out.println();
 			System.out.println(subrubroABM.traerSubrubro(5));
 			System.out.println();
 			
+			CamareroABM camareroABM = new CamareroABM();
+			
+			System.out.println(camareroABM.traerCamarero(1));
+			System.out.println(camareroABM.traerCamarero(2));
+			System.out.println();
+			
+			RestaurantABM restaurantABM = new RestaurantABM();
+			
+			System.out.println(restaurantABM.traerRestaurant(1));
+					
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
